5ca4ce778a80495fd20621162ddbf37ef35d8ce1
hkonate <konate.tronca@gmail.com>
2023-06-07 15:46:41 +0200

    step 3 done

--- html_template/js/script.js
+++ html_template/js/script.js
@@ -1,6 +1,14 @@
-const userName = prompt("Quel est votre prénom ?");
-const hello = `👋 Bonjour ${userName}`;
-document.body.innerHTML += `<h2>${hello}</h2>`;
-const birth = prompt("Quel est votre année de naissance ?");
-const age = 2023 - birth;
-document.body.innerHTML += `<h3>Vous avez ${age} ans</h3>`;
+function askName() {
+  const userName = prompt("Quel est votre prénom ?");
+  const hello = `👋 Bonjour ${userName}`;
+  document.body.innerHTML += `<h2>${hello}</h2>`;
+}
+
+function askBirthYear() {
+  const birth = prompt("Quel est votre année de naissance ?");
+  const age = 2023 - birth;
+  document.body.innerHTML += `<h3>Vous avez ${age} ans</h3>`;
+}
+
+askName();
+askBirthYear();
