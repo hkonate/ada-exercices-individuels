a85a7bf144f198b5b3a946a1ac578b2f56ee4b7b
hkonate <konate.tronca@gmail.com>
2023-06-07 21:57:33 +0200

    bonus step

--- html_template/js/script.js
+++ html_template/js/script.js
@@ -5,8 +5,30 @@ function askName() {
 }
 
 function askBirthYear() {
+  const arrOfMonths = [
+    "janvier",
+    "février",
+    "mars",
+    "avril",
+    "mai",
+    "juin",
+    "juillet",
+    "août",
+    "septembre",
+    "octobre",
+    "novembre",
+    "décembre",
+  ];
   const birth = prompt("Quel est votre année de naissance ?");
-  const age = 2023 - birth;
+  const month = prompt("Quel est votre mois de naissance ?");
+
+  let age = 2023 - birth;
+
+  if (
+    arrOfMonths.includes(month.toLowerCase()) &&
+    arrOfMonths.indexOf(month.toLowerCase()) + 1 >= 6
+  )
+    age--;
   document.body.innerHTML += `<h3>Vous avez ${age} ans</h3>`;
 }
 
